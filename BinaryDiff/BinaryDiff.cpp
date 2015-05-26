8058deb87a12dcd1ed440f2cf834c9451a1eaf11
Adam Spindler <xmeadamx@gmail.com>
2015-05-26 13:33:26 -0400

    Initial commit

---
+++ BinaryDiff/BinaryDiff.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	string fileAPath = "";
+	string fileBPath = "";
+	return 0;
+}
